97a2d2e750ba41ad5468bceac848cb9c2e1d4e7e
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2018-10-08 01:15:55 +0200

    started off with laptop dotfiles arch 2018-10-08

---
+++ i3/thank god.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output DVI-I-1 --primary --mode 1920x1080 --pos 1280x840 --rotate normal --output DVI-D-1 --mode 1280x1024 --pos 0x896 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 3200x0 --rotate left
